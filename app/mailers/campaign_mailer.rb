57deea22bdbfff486520265bb6c928c8d5c31d80
Claudinei Ap. Perboni <cperbony@gmail.com>
2019-06-13 22:43:15 -0300

    update mailer campaign

--- app/mailers/campaign_mailer.rb
+++ app/mailers/campaign_mailer.rb
@@ -6,8 +6,9 @@ class CampaignMailer < ApplicationMailer
   #   en.campaign_mailer.raffle.subject
   #
   def raffle
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+    @campaign = campaign
+    @member = member
+    @friend = friend
+    mail to: @member.email, subject: "Nosso Amigo Secreto: #{@campaign.title}"
   end
 end
